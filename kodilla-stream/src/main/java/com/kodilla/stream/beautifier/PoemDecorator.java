726d0fa7ebb5605f658d0cffc7390f41cfdd1acc
Sebastian Lachut <seb.lachut@gmail.com>
2024-01-23 20:16:25 +0100

    Created simple usage of lambda functions

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemDecorator.java
@@ -0,0 +1,6 @@
+package com.kodilla.stream.beautifier;
+
+public interface PoemDecorator {
+
+    void decorate(String b);
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/ExecuteSaySomething.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.lambda;
+
+public class ExecuteSaySomething implements Executor{
+
+    @Override
+    public void process() {
+        System.out.println("Example text");
+    }
+
+
+}
