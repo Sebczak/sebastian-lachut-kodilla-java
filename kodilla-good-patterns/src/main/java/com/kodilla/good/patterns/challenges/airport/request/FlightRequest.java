28c267810900fe13bec00183ccde0a87eef9a65b
Sebastian Lachut <seb.lachut@gmail.com>
2024-02-27 19:32:57 +0100

    12.5 Fix findFlightsThroughCity

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/airport/request/FlightRequest.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/airport/request/FlightRequest.java
@@ -4,14 +4,20 @@ public class FlightRequest {
 
     private String departureCity;
     private String destinationCity;
+    private String planeStopCity;
 
-    public FlightRequest(String departureCity, String destinationCity) {
+    public FlightRequest(String departureCity, String planeStopCity, String destinationCity) {
         this.departureCity = departureCity;
+        this.planeStopCity = planeStopCity;
         this.destinationCity = destinationCity;
     }
 
     public String getDepartureCity() {
         return departureCity;
+    }
+
+    public String getPlaneStopCity() {
+        return planeStopCity;
     }
 
     public String getDestinationCity() {
