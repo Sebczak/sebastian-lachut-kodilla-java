b58a86c396053ff98ec4589724dad534868e8f97
Sebastian Lachut <seb.lachut@gmail.com>
2024-02-28 20:47:25 +0100

    13.1 Spring indroduction

---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.spring.intro.shape;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DrawerTestSuite {
+
+    @Test
+    void doDrawingWithACircle() {
+        //Given
+        Circle circle = new Circle();
+
+        //When
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+
+        //Then
+        assertEquals("Circle", result);
+    }
+
+    @Test
+    void doDrawingWithATriangle() {
+        //Given
+        Triangle triangle = new Triangle();
+
+        //When
+        Drawer drawer = new Drawer(triangle);
+        String result = drawer.doDrawing();
+
+        //Then
+        assertEquals("Triangle", result);
+    }
+}
