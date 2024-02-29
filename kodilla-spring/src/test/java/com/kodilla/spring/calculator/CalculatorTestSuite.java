f79e46baa616cfd51f72b12b1a00c18dc7048a2a
Sebastian Lachut <seb.lachut@gmail.com>
2024-02-29 16:15:55 +0100

    13.2 Create usefull tests

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class CalculatorTestSuite {
 
@@ -15,10 +17,17 @@ public class CalculatorTestSuite {
         //Given
 
         //When
-        calculator.add(2,3);
-        calculator.sub(2,3);
-        calculator.mul(2,3);
-        calculator.div(2,0);
-        calculator.div(2,3);
+        Double addingResult = calculator.add(2,3);
+        Double subtractResult = calculator.sub(2,3);
+        Double multiplyResult = calculator.mul(2,3);
+        Double divideBy0Result = calculator.div(2,0);
+        Double divideResult = calculator.div(2,4);
+
+        //Then
+        assertEquals(5, addingResult);
+        assertEquals(-1, subtractResult);
+        assertEquals(6, multiplyResult);
+        assertEquals(1, divideBy0Result);
+        assertEquals(0.5, divideResult);
     }
 }
